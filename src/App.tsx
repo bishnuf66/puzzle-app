f30cea8edab54269a88f3b623a2f38f43fa83562
bishnuf66 <bkbishnu2000@gmail.com>
2024-12-14 12:27:33 +0545

    added dotenv to load environment variables and added react-toastify to show errors, info and other  messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
+import Login from "./components/Login";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <div className="bg-blue-800 text-pink-400">puzzle app </div>;
+  return (
+    <div>
+      <Login />
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
+import dotenv from 'dotenv';
+
 import { defineConfig } from 'vite'
+
+// Load .env file variables
+dotenv.config();
+
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
