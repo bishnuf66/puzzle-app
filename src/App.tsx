748f515ed86b1a3070125ae21701cb495a510696
bishnuf66 <bkbishnu2000@gmail.com>
2024-12-14 23:48:14 +0545

    added route to dashboard and main puzzle game compoenent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { ToastContainer } from "react-toastify";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import "react-toastify/dist/ReactToastify.css";
+import PuzzleGame from "./components/PuzzleGame";
+import DashBoard from "./components/DashBoard";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <div>
-      <Login />
-      <ToastContainer />
-    </div>
+    <>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/game" element={<PuzzleGame />} />
+            <Route path="/dashboard" element={<DashBoard />} />
+          </Routes>
+        </Router>
+        <ToastContainer />
+      </AuthProvider>
+    </>
   );
 }
 
