ad16003081e9b850f0f5dfd5980efded483757a9
bishnuf66 <bkbishnu2000@gmail.com>
2024-12-17 01:04:57 +0545

    added private route compoenent to portect compoenent

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+interface PrivateRouteProps {
+  element: React.ReactNode;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ element }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <>{element}</>;
+};
+
+export default PrivateRoute;
